2396599dc542b00abcbf9cfb90194d8f2242d684
Sujith G <sujith3g@gmail.com>
2015-06-14 11:39:41 +0530

    repo details added

--- package.js
+++ package.js
@@ -6,7 +6,10 @@
 * @Email: sujith3g@gmail.com
 */
 Package.describe({
-summary: "jsTree 3.1.1 for meteor"
+summary: "jsTree 3.1.1 for meteor",
+version: '3.1.1',
+name: 'sujith3g:jstree',
+git: 'https://github.com/sujith3g/jstree'
 });
 Package.on_use(function (api, where) {
 	api.use(['jquery'], 'client');
